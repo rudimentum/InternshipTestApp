3ee2dc561be2a8cd0bf6d6fb504927bb88f3ec70
Victoria Grinberg <31312528+rudimentum@users.noreply.github.com>
2022-04-26 17:26:07 +0300

    Add RecyclerView

---
+++ app/src/main/java/com/rudimentum/internshiptestapp/adapter/CustomRecyclerAdapter.kt
@@ -0,0 +1,33 @@
+package com.rudimentum.internshiptestapp.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.rudimentum.internshiptestapp.HomeFeed
+import com.rudimentum.internshiptestapp.R
+import kotlinx.android.synthetic.main.recyclerview_item.view.*
+
+class CustomRecyclerAdapter(private val homeFeed: HomeFeed) : RecyclerView
+.Adapter<CustomRecyclerAdapter.MyViewHolder>() {
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.recyclerview_item, parent, false)
+        return MyViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val character = homeFeed.characters.get(position)
+        holder.itemView.textViewLarge.text = character.name
+        holder.itemView.textViewSmall.text = character.status
+
+    }
+
+    override fun getItemCount(): Int {
+        return 0
+    }
+}
