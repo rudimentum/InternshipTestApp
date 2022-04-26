f142c000b4f94c8444e13364c78248361222240b
Victoria Grinberg <31312528+rudimentum@users.noreply.github.com>
2022-04-26 17:26:51 +0300

    Add http get request

--- app/src/main/java/com/rudimentum/internshiptestapp/MainActivity.kt
+++ app/src/main/java/com/rudimentum/internshiptestapp/MainActivity.kt
@@ -2,10 +2,58 @@ package com.rudimentum.internshiptestapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.GsonBuilder
+import com.rudimentum.internshiptestapp.adapter.CustomRecyclerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        fetchJSON()
     }
+
+    private fun fillList(): List<String> {
+        val data = mutableListOf<String>()
+        (0..30).forEach { i -> data.add("$i element") }
+        return data
+    }
+    fun fetchJSON() {
+        val url = "https://rickandmortyapi.com/api/character"
+        val client = OkHttpClient()
+        val request = Request.Builder().url(url).build()
+        client.newCall(request).enqueue(object: Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                println("Failed to execute request")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val body = response?.body?.string()
+                println(body)
+
+                val gson = GsonBuilder().create()
+                val homeFeed = gson.fromJson(body, HomeFeed::class.java)
+
+                runOnUiThread {
+                    recyclerView.adapter = CustomRecyclerAdapter(homeFeed)
+                }
+            }
+        })
+    }
+
 }
+
+class HomeFeed(val characters: List<Character>)
+
+class Character(val id: Int,
+                val name: String,
+                val status: String)
